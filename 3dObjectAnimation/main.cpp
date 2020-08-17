6c3e954eaa14597346fe8f2b11cd59f51c47a28e
HarewVlad <HarewVlad@mail.ru>
2020-08-17 23:51:57 +0300

    Removed

--- 3dObjectAnimation/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "game.h"
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
-  Game *game = new Game {};
-  game->init();
-  game->run();
-}
